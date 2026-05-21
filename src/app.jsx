8b66ae031d042ccc03843d81b0ee88116d3f5a5d
links743 <lincolnsivlanduox83@gmail.com>
2026-05-21 21:32:18 +1000

    Create app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center", fontFamily: "sans-serif" }}>
+      <h1>Melo Transport Services</h1>
+      <p>Website is working!</p>
+    </div>
+  )
+}
